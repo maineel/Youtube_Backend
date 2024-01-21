a841b67d298be51d3be347d345830963d86d0b83
neel <neelxdxd@gmail.com>
2024-01-21 12:50:23 +0530

    Api error and Api response

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import express from "express";
+const app = express();
 
 dotenv.config({
-    path: "./.env"
+    path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error",(error) => {console.log("ERROR: ", error);
+            throw error
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on port: ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log("MONGO db connection failed", error);
+});
 
 /*
 import express from "express";
@@ -16,7 +29,7 @@ const app = express();
 (async() => {
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",() => {console.log("ERROR: ", error);
+        app.on("error",(error) => {console.log("ERROR: ", error);
             throw error
         })
         app.listen(process.env.PORT, () => {
@@ -24,7 +37,7 @@ const app = express();
         })
     }catch(error){
         console.error("ERROR: ",error);
-        throw err
+        throw error
     }
 })()
 */
