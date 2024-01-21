748ca9bb4575a0d186eba85802aa58b36a14f5b8
neel <neelxdxd@gmail.com>
2024-01-21 20:16:36 +0530

    User register routes & controller

--- src/app.js
+++ src/app.js
@@ -12,15 +12,15 @@ app.use(cors({
 app.use(express.json({
     limit: "16kb",
 }));
-
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
 
+// ROUTES import
+import userRouter from './routes/user.routes.js'
 
-
+// ROUTES declaration
+app.use("/api/v1/users", userRouter)
 
 
 
